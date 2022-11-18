ccc7c6243929ca11e7d0a76dc8608edfd5d2e642
yhuang060122 <106745168+yhuang060122@users.noreply.github.com>
2022-11-18 22:56:33 +0100

    TennisGame1 - extract methods

--- Tennis/TennisGame1.cs
+++ Tennis/TennisGame1.cs
@@ -4,8 +4,8 @@ namespace Tennis
     {
         private int m_score1 = 0;
         private int m_score2 = 0;
-        private string player1Name;
-        private string player2Name;
+        private readonly string player1Name;
+        private readonly string player2Name;
 
         public TennisGame1(string player1Name, string player2Name)
         {
@@ -15,7 +15,7 @@ namespace Tennis
 
         public void WonPoint(string playerName)
         {
-            if (playerName == "player1")
+            if (playerName == this.player1Name)
                 m_score1 += 1;
             else
                 m_score2 += 1;
@@ -23,60 +23,76 @@ namespace Tennis
 
         public string GetScore()
         {
-            string score = "";
-            var tempScore = 0;
+            string score;
             if (m_score1 == m_score2)
             {
-                switch (m_score1)
-                {
-                    case 0:
-                        score = "Love-All";
-                        break;
-                    case 1:
-                        score = "Fifteen-All";
-                        break;
-                    case 2:
-                        score = "Thirty-All";
-                        break;
-                    default:
-                        score = "Deuce";
-                        break;
-
-                }
+                score = GetEqualScore(m_score1);
             }
             else if (m_score1 >= 4 || m_score2 >= 4)
             {
-                var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+                score = GetWinnerOrAdvantageScore(m_score1, m_score2);
             }
             else
             {
-                for (var i = 1; i < 3; i++)
-                {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
-                    switch (tempScore)
-                    {
-                        case 0:
-                            score += "Love";
-                            break;
-                        case 1:
-                            score += "Fifteen";
-                            break;
-                        case 2:
-                            score += "Thirty";
-                            break;
-                        case 3:
-                            score += "Forty";
-                            break;
-                    }
-                }
+                score = GetNomalScore(m_score1, m_score2);
             }
             return score;
         }
+
+        private static string GetNomalScore(int m_score1, int m_score2)
+        {
+            string score = "";
+            for (var i = 1; i < 3; i++)
+            {
+                int tempScore;
+                if (i == 1) tempScore = m_score1;
+                else { score += "-"; tempScore = m_score2; }
+                switch (tempScore)
+                {
+                    case 0:
+                        score += "Love";
+                        break;
+                    case 1:
+                        score += "Fifteen";
+                        break;
+                    case 2:
+                        score += "Thirty";
+                        break;
+                    case 3:
+                        score += "Forty";
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return score;
+        }
+
+        private static string GetWinnerOrAdvantageScore(int m_score1, int m_score2)
+        {
+            string score;
+            var minusResult = m_score1 - m_score2;
+            if (minusResult == 1) score = "Advantage player1";
+            else if (minusResult == -1) score = "Advantage player2";
+            else if (minusResult >= 2) score = "Win for player1";
+            else score = "Win for player2";
+            return score;
+        }
+
+        private static string GetEqualScore(int m_score)
+        {
+            string score = m_score switch
+            {
+                0 => "Love-All",
+                1 => "Fifteen-All",
+                2 => "Thirty-All",
+                _ => "Deuce",
+            };
+            return score;
+        }
+
+
     }
 }
 
