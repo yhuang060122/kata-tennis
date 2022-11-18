4b461fd4508344486f783df1aff8924d0d101cfe
yhuang060122 <106745168+yhuang060122@users.noreply.github.com>
2022-11-19 00:10:50 +0100

    TennisGame2 - Refactor GetScore()

--- Tennis/TennisGame2.cs
+++ Tennis/TennisGame2.cs
@@ -7,8 +7,6 @@ namespace Tennis
         private int p1point = 0;
         private int p2point = 0;
 
-        private string p1res = "";
-        private string p2res = "";
         private readonly string player1Name;
         private readonly string player2Name;
 
@@ -28,28 +26,22 @@ namespace Tennis
 
         public string GetScore()
         {
-            // For Equal scores
-            string score = GetEqualScore();
+            if (HasWinner)
+            {
+                return GetWinnerScore();
+            }
 
-            if (!string.IsNullOrEmpty(score))
-                return score;
+            if (IsEqualScore)
+            {
+                return GetEqualScore();
+            }
 
-            // For Has a winner
-            score = GetWinnerScore();
+            if (HasAdvantage)
+            {
+                return GetAdvantageScore();
+            }
 
-            if (!string.IsNullOrEmpty(score))
-                return score;
-
-            // For Has advantage
-            score = GetAdvantageScore();
-
-            if (!string.IsNullOrEmpty(score))
-                return score;
-
-            // For Normal scores
-            score = GetNormalScore();
-
-            return score;
+            return GetNormalScore();
         }
 
         private string GetNormalScore()
@@ -69,64 +61,44 @@ namespace Tennis
 
         private string GetWinnerScore()
         {
-            string score = "";
-
-            if (HasWinner)
-            {
-                score = p1point > p2point ? $"Win for {player1Name}" : $"Win for {player2Name}";
-            }
-
-            return score;
+            return p1point > p2point ? $"Win for {player1Name}" : $"Win for {player2Name}";
         }
 
         private string GetAdvantageScore()
         {
-            string score = "";
-
-            if (HasAdvantage)
-            {
-                if (p1point > p2point)
-                {
-                    score = $"Advantage {player1Name}";
-                }
-                else
-                {
-                    score = $"Advantage {player2Name}";
-                }
-            }
-
-            return score;
+            return p1point > p2point ? $"Advantage {player1Name}" : $"Advantage {player2Name}";
         }
 
         private string GetEqualScore()
         {
-            string score = "";
-            if (IsEqualScore)
+            string score;
+            if (p1point < 3)
             {
-                if (p1point < 3)
+                score = p1point switch
                 {
-                    score = p1point switch
-                    {
-                        0 => "Love",
-                        1 => "Fifteen",
-                        2 => "Thirty",
-                        _ => "",
-                    };
-                    score += "-All";
-                }
-                else
-                {
-                    score = "Deuce";
-                }
+                    0 => "Love",
+                    1 => "Fifteen",
+                    2 => "Thirty",
+                    _ => "",
+                };
+                score += "-All";
+            }
+            else
+            {
+                score = "Deuce";
             }
             return score;
         }
 
-        private bool IsEqualScore => p1point == p2point;
+        private bool IsEqualScore => DiffPoints == 0;
 
-        private bool HasAdvantage => Math.Abs(p1point - p2point) == 1 && (p1point >= 4 || p2point >= 4);
+        private bool HasAdvantage => DiffPoints == 1 && HasPlayerAtLeastFourWins;
 
-        private bool HasWinner => Math.Abs(p1point - p2point) > 1 && (p1point >= 4 || p2point >= 4);
+        private bool HasWinner => DiffPoints > 1 && HasPlayerAtLeastFourWins;
+
+        private bool HasPlayerAtLeastFourWins => (p1point >= 4 || p2point >= 4);
+
+        private int DiffPoints => Math.Abs(p1point - p2point);
     }
 }
 
