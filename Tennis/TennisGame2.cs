c9889da6661e8f25c638becc9bfd1b3d21ed117d
yhuang060122 <106745168+yhuang060122@users.noreply.github.com>
2022-11-18 23:26:57 +0100

    TennisGame2 - First Refactor

--- Tennis/TennisGame2.cs
+++ Tennis/TennisGame2.cs
@@ -2,19 +2,26 @@ namespace Tennis
 {
     public class TennisGame2 : ITennisGame
     {
-        private int p1point;
-        private int p2point;
+        private int p1point = 0;
+        private int p2point = 0;
 
         private string p1res = "";
         private string p2res = "";
-        private string player1Name;
-        private string player2Name;
+        private readonly string player1Name;
+        private readonly string player2Name;
 
         public TennisGame2(string player1Name, string player2Name)
         {
             this.player1Name = player1Name;
-            p1point = 0;
             this.player2Name = player2Name;
+        }
+
+        public void WonPoint(string player)
+        {
+            if (player == player1Name)
+                p1point++;
+            else
+                p2point++;
         }
 
         public string GetScore()
@@ -85,57 +92,23 @@ namespace Tennis
 
             if (p1point > p2point && p2point >= 3)
             {
-                score = "Advantage player1";
+                score = $"Advantage {player1Name}";
             }
 
             if (p2point > p1point && p1point >= 3)
             {
-                score = "Advantage player2";
+                score = $"Advantage {player2Name}";
             }
 
             if (p1point >= 4 && p2point >= 0 && (p1point - p2point) >= 2)
             {
-                score = "Win for player1";
+                score = $"Win for {player1Name}";
             }
             if (p2point >= 4 && p1point >= 0 && (p2point - p1point) >= 2)
             {
-                score = "Win for player2";
+                score = $"Win for {player2Name}";
             }
             return score;
-        }
-
-        public void SetP1Score(int number)
-        {
-            for (int i = 0; i < number; i++)
-            {
-                P1Score();
-            }
-        }
-
-        public void SetP2Score(int number)
-        {
-            for (var i = 0; i < number; i++)
-            {
-                P2Score();
-            }
-        }
-
-        private void P1Score()
-        {
-            p1point++;
-        }
-
-        private void P2Score()
-        {
-            p2point++;
-        }
-
-        public void WonPoint(string player)
-        {
-            if (player == "player1")
-                P1Score();
-            else
-                P2Score();
         }
 
     }
